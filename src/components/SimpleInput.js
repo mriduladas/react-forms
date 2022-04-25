b7176d707b0eb442e5ccef077e35d16d0a503d26
mriduladas <mridula199@gmail.com>
2022-04-25 21:57:57 +0530

    Reduced the no of lines of code

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -4,47 +4,48 @@ import {useState } from "react";
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName]= useState('');
   //const namedInputRef = useRef();
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enterNameTouched, setEnteredNameTouched] = useState(false);
+  const enteredNameIsValid = enteredName.trim().length > 0;
+  const enterNameIsInvalid = !enteredNameIsValid && enterNameTouched;
 
   const onChangeHandler= (event) => {
     console.log(event.target.value);
     setEnteredName(event.target.value);
-    if(event.target.value.trim().length > 0){
-      setEnteredNameIsValid(true);
-      //return
-    }
+    // if(event.target.value.trim().length > 0){
+    //   setEnteredNameIsValid(true);
+    //   //return
+    // }
   }
 
   const onBlurHandler = (event)=>{
     setEnteredNameTouched(true);
-    if(enteredName.trim().length === 0){
-      setEnteredNameIsValid(false);
-      //return
-    }
+    // if(enteredName.trim().length === 0){
+    //   setEnteredNameIsValid(false);
+    //   //return
+    // }
     //setEnteredNameIsValid(true);
   }
 
   const onSubmitHandler = (event)=> {
     event.preventDefault();
     setEnteredNameTouched(true);
-    if(enteredName.trim().length === 0){
-      setEnteredNameIsValid(false);
+    if(!enteredNameIsValid){
+      
       return
     }
-    setEnteredNameIsValid(true);
     
     // const enteredValue = namedInputRef.current.value;
     console.log(event.target.value);
     //Clear the input field on click on submit using State Advisable
     setEnteredName('');
+    setEnteredNameTouched(false);
 
     //clear the input field on click on submit using ref NOT-ADVISABLE
     // namedInputRef.current.value =''
 
   }
 
-  const enterNameIsInvalid = !enteredNameIsValid && enterNameTouched;
+
   const namedInputClasses = enterNameIsInvalid ? 'form-control invalid' : 'form-control'
   return (
     <form onSubmit={onSubmitHandler}>
