badcd10c06446570f895459a0404c982924a759d
mriduladas <mridula199@gmail.com>
2022-04-25 23:10:28 +0530

    Using another input field in SimpleInput Form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -5,12 +5,30 @@ const SimpleInput = (props) => {
   const [enteredName, setEnteredName]= useState('');
   //const namedInputRef = useRef();
   const [enterNameTouched, setEnteredNameTouched] = useState(false);
+
+  const [enteredEmail, setEnteredEmail]= useState('');
+  const [enterEmailTouched, setEnteredEmailTouched] = useState(false);
+
   const enteredNameIsValid = enteredName.trim().length > 0;
   const enterNameIsInvalid = !enteredNameIsValid && enterNameTouched;
 
+  const enteredEmailIsValid = enteredEmail.trim().includes('@');
+  const enterEmailIsInvalid = !enteredEmailIsValid && enterEmailTouched;
+
+  const formValid = enteredNameIsValid && enteredEmailIsValid;
+
   const onChangeHandler= (event) => {
-    console.log(event.target.value);
+    console.log("name",event.target.value);
     setEnteredName(event.target.value);
+    // if(event.target.value.trim().length > 0){
+    //   setEnteredNameIsValid(true);
+    //   //return
+    // }
+  }
+
+  const onEmailChangeHandler= (event) => {
+    console.log("Email",event.target.value);
+    setEnteredEmail(event.target.value);
     // if(event.target.value.trim().length > 0){
     //   setEnteredNameIsValid(true);
     //   //return
@@ -26,10 +44,15 @@ const SimpleInput = (props) => {
     //setEnteredNameIsValid(true);
   }
 
+  const onBlurEmailHandler = (event)=>{
+    setEnteredEmailTouched(true);
+  }
+
   const onSubmitHandler = (event)=> {
     event.preventDefault();
     setEnteredNameTouched(true);
-    if(!enteredNameIsValid){
+    setEnteredEmailTouched(true);
+    if(!formValid){
       
       return
     }
@@ -39,6 +62,8 @@ const SimpleInput = (props) => {
     //Clear the input field on click on submit using State Advisable
     setEnteredName('');
     setEnteredNameTouched(false);
+    setEnteredEmail('');
+    setEnteredEmailTouched(false);
 
     //clear the input field on click on submit using ref NOT-ADVISABLE
     // namedInputRef.current.value =''
@@ -46,7 +71,8 @@ const SimpleInput = (props) => {
   }
 
 
-  const namedInputClasses = enterNameIsInvalid ? 'form-control invalid' : 'form-control'
+  const namedInputClasses = enterNameIsInvalid ? 'form-control invalid' : 'form-control';
+  const emailInputClasses = enterEmailIsInvalid ? 'form-control invalid' : 'form-control';
   return (
     <form onSubmit={onSubmitHandler}>
       <div className={namedInputClasses}>
@@ -54,8 +80,13 @@ const SimpleInput = (props) => {
         <input type='text' id='name' onChange={onChangeHandler}  value={enteredName} onBlur={onBlurHandler} />
         {enterNameIsInvalid && <p className="error-text">Input Name cannot be Blanks.</p>}
       </div>
+      <div className={emailInputClasses}>
+        <label htmlFor='email'>Your Email</label>
+        <input type='email' id='email' onChange={onEmailChangeHandler}  value={enteredEmail} onBlur={onBlurEmailHandler} />
+        {enterEmailIsInvalid && <p className="error-text">Input Email cannot be Blanks.</p>}
+      </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formValid}>Submit</button>
       </div>
     </form>
   );
