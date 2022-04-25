35dd4b95336e05d0f877234dca369e9335a608e3
mriduladas <mridula199@gmail.com>
2022-04-25 17:14:24 +0530

    clear the input value using ref NOT-Advisable

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -14,15 +14,18 @@ const SimpleInput = (props) => {
     event.preventDefault();
     const enteredValue = namedInputRef.current.value;
     console.log(enteredValue);
-    //Clear the input field on click on submit
-    setEnteredName('');
+    //Clear the input field on click on submit using State Advisable
+    //setEnteredName('');
+
+    //clear the input field on click on submit using ref NOT-ADVISABLE
+    namedInputRef.current.value =''
 
   }
   return (
     <form onSubmit={onSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={onChangeHandler} ref={namedInputRef} value={enteredName} />
+        <input type='text' id='name' onChange={onChangeHandler} ref={namedInputRef}  />
       </div>
       <div className="form-actions">
         <button>Submit</button>
