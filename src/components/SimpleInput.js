eabbfdd03a6d2fc8fa526a5ab57327c5dd8fee1f
mriduladas <mridula199@gmail.com>
2022-04-25 17:06:04 +0530

    Fetch user Input on every key Stroke

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName]= useState('');
+
+  const onChangeHandler= (event) => {
+    console.log(event.target.value);
+    setEnteredName(event.target.value);
+  }
   return (
     <form>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={onChangeHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
